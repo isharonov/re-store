4ef91266ccc2024246374e25e2fedcb70c506664
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-19 18:36:34 +0300

    Self work: add Spinner component

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./spinner.css";
+
+const Spinner = () => {
+  return (
+    <div className="loadingio-spinner-pulse-rba71pro6jd">
+      <div className="ldio-e7jud38ptrl">
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
