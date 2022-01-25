32b061a3f7ec445980c1d4aba2e97a074e131703
Ivan Sharonov <48245524+isharonov@users.noreply.github.com>
2022-01-25 19:36:57 +0300

    12-138: Styles (optional lesson)

---
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./shopping-cart-table.css";
+
+const ShoppingCartTable = () => {
+  return (
+    <div className="shopping-cart-table">
+      <h2>Your Order</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Item</th>
+            <th>Count</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Site Reliability Something Interesting</td>
+            <td>2</td>
+            <td>$40</td>
+            <td>
+              <button className="btn btn-outline-danger">
+                <i className="fa fa-trash-o"></i>
+              </button>
+              <button className="btn btn-outline-success">
+                <i className="fa fa-plus-circle"></i>
+              </button>
+              <button className="btn btn-outline-warning">
+                <i className="fa fa-minus-circle"></i>
+              </button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div className="total">
+        Total: $201
+      </div>
+    </div>
+  );
+};
+
+export default ShoppingCartTable;
